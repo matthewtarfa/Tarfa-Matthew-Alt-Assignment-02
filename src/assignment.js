574f8307b12ab2a3aa632a5cc8c1673ac336aece
Matthew M Tarfa <105323757+matthewtarfa@users.noreply.github.com>
2022-07-05 19:27:08 +0000

    Made changes in challenge -1 and challenge -3

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    let sum = 0;
+
+    for (i = 0; i < arrayOfNumbers.length; i++){
+
+        sum += arrayOfNumbers[i]
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -47,9 +54,16 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+
+    const arrOfFarenheit = []
+
+    for (i = 0; i < arrayOfNumbers.length; i++){
+        let temp = Math.floor(arrayOfNumbers [i] * 9/5) +32;
+        arrOfFarenheit.push(temp);
+    }
+    return arrOfFarenheit;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
